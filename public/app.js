9f01a62891b820573e5cf7423a155185eabfe869
Ada <adaselcuk16@gmail.com>
2024-06-29 16:43:54 +0300

    added error messages to code. not showing on live server but will add server.js script and see if it needs more debugging

--- public/app.js
+++ public/app.js
@@ -1,3 +1,5 @@
+console.log('Script loaded');
+
 document.getElementById('uploadLabel').addEventListener('click', function() {
 	document.getElementById('imageUpload').click();
 })
@@ -5,10 +7,49 @@ document.getElementById('uploadLabel').addEventListener('click', function() {
 document.getElementById('imageUpload').addEventListener('change', function() {
 	var fileName = this.files[0] ? this.files[0].name : "";
 
+	if (this.files.length > 0) {
+		console.log("File selected: " + fileName);
+	}
+
 	if (fileName) {
 		document.getElementById('uploadLabel').textContent = "File selected: " + fileName;
 	}
 	else {
 		document.getElementById('uploadLabel').textContent = "Please choose a file";
+	}
+});
+
+document.getElementById('uploadForm').addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    console.log('Form submitted');
+    var fileInput = document.getElementById('imageUpload');
+    var errorMessageElement = document.getElementById('error-message');
+
+	errorMessageElement.textContent= '';
+
+    if (fileInput.files.length == 0) {
+		errorMessageElement.textContent = 'Please upload a file before submitting the form';
+		return;
+	}
+	const formData = new FormData(event.target);
+	
+
+	try{ 
+		const response = await fetch('/upload', {
+			method: 'POST',
+			body: formData
+		});
+
+		if (!response.ok) {
+			throw new Error('Network response was not ok');
+		}
+
+		const result = await response.json();
+		document.getElementById('result').textContent = JSON.stringify(result, null, 2);
+	}
+	catch (error) {
+		console.error('Fetch error:', error);
+		errorMessageElement.textContent = 'Failed to upload the file. Please try again.';
 	}
 });
